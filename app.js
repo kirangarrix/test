2aa26fae6bb8f75c7bc29e20824f9b0e8ac7aba1
Akhil-EM <akhilemcalicut@gmail.com>
2021-10-28 13:21:29 +0530

    tracking heroku error7

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-// const path = require("path");
-// const morgan = require("morgan")
+const path = require("path");
+const morgan = require("morgan")
 
 const port = process.env.PORT || 5050;
 var app = express();
 
-// // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
-// app.use(morgan("tiny")); //logs request-endpoint and time taken
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("tiny")); //logs request-endpoint and time taken
+app.use(express.static(path.join(__dirname, "public")));
 
 
-// //view routers
-// // app.use("/", require("./routes/route"));
-
-// app.get("/",function(req,res){
-//   res.send("ima ahere ")
-// })
-
-
-
-// //no router found will trigger this by default
-// app.all("*", (req, res) => {
-//   res.render("error");
-// });
+//view routers
+app.use("/", require("./routes/route"));
 
 app.get("/",function(req,res){
-     console.log("calling index");
-     res.send("on index page");
+  res.send("ima ahere ")
+})
+
+
+
+//no router found will trigger this by default
+app.all("*", (req, res) => {
+  res.render("error");
 });
+
+
 app.listen(port, () => console.log(`\napplication is running at ${port}`));
 // app.listen(() => console.log(`\napplication is running at ${port}`));
